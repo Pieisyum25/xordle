cf40e2a886588b259304e57b62cd86855f184a04
John Flynn <johnhfflynn25@gmail.com>
2022-04-01 01:20:55 +1300

    Add component hierarchy
    The UI has been broken down into a hierarchy of components representing its structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,225 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GameBar />
+      <GameArea />
     </div>
   );
 }
 
 export default App;
+
+// GameBar: =============================================================================================
+
+function GameBar() {
+  return (
+    <div>
+      <GameBarGroupLeft />
+      <GameBarGroupRight />
+    </div>
+  );
+}
+
+function GameBarGroup(props){
+  return (
+    <div>
+      {props.children}
+    </div>
+  );
+}
+
+function GameBarGroupLeft(){
+  return (
+    <div>
+      <GameBarGroup>
+        <IconImage />
+        <TitleLabel />
+      </GameBarGroup>
+    </div>
+  );
+}
+
+function IconImage(){
+  return (
+    <div>
+      <img src='logo.svg' alt='Xordle Icon' />
+    </div>
+  );
+}
+
+function TitleLabel(){
+  return (
+    <div>
+      Xordle
+    </div>
+  );
+}
+
+function GameBarGroupRight(){
+  return (
+    <div>
+      <GameBarGroup>
+        <WordCountBar />
+        <GameBarButton desc='Help' onClick={alert} imgSrc='logo.svg'/>
+      </GameBarGroup>
+    </div>
+  );
+}
+
+function WordCountBar(){
+  return (
+    <div>
+      <WordCountLabel wordCount={'X'} />
+      <GameBarButton desc='Increment Word Count' onClick={alert} imgSrc='logo.svg' />
+      <GameBarButton desc='Decrement Word Count' onClick={alert} imgSrc='logo.svg' />
+    </div>
+  );
+}
+
+function WordCountLabel(props){
+  return (
+    <div>
+      Words: {props.wordCount}
+    </div>
+  );
+}
+
+function GameBarButton(props){
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        <img src={props.imgSrc} alt={props.desc + 'Button Image'} />
+      </button>
+    </div>
+  );
+}
+
+
+// GameArea: ========================================================================================================
+
+function GameArea(){
+  return (
+    <div>
+      <AllWordsGrid />
+      <KeyboardGrid />
+    </div>
+  );
+}
+
+function AllWordsGrid(){
+  return (
+    <div>
+      <ul>
+        <li><OneWordGrid /></li>
+        <li><OneWordGrid /></li>
+        <li><OneWordGrid /></li>
+        <li><OneWordGrid /></li>
+      </ul>
+    </div>
+  );
+}
+
+function OneWordGrid(){
+  return (
+    <div>
+      <ul>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+        <li><GuessRow /></li>
+      </ul>
+    </div>
+  );
+}
+
+function GuessRow(){
+  return (
+    <div>
+      <ul>
+        <li><LetterCell /></li>
+        <li><LetterCell /></li>
+        <li><LetterCell /></li>
+        <li><LetterCell /></li>
+        <li><LetterCell /></li>
+      </ul>
+    </div>
+  );
+}
+
+function LetterCell(){
+  return (
+    <div>
+      X
+    </div>
+  );
+}
+
+function KeyboardGrid(){
+  return (
+    <div>
+      <ul>
+        <li><KeyboardRow /></li>
+        <li><KeyboardRow /></li>
+        <li><KeyboardRow /></li>
+      </ul>
+    </div>
+  );
+}
+
+function KeyboardRow(){
+  return (
+    <div>
+      <ul>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+        <li><KeyboardCell /></li>
+      </ul>
+    </div>
+  );
+}
+
+function KeyboardCell(){
+  return (
+    <div>
+      <button>
+        <LetterCell />
+      </button>
+    </div>
+  );
+}
